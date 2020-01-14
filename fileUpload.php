9c82fe201f03d945b26031b003ce8dace94acf86
Karel J. Park <kalriel1@gmail.com>
2020-01-14 21:57:12 +0900

    Modifying upload

--- fileUpload.php
+++ fileUpload.php
@@ -10,7 +10,7 @@ move_uploaded_file($SOURCE, $DEST);
         <title>Profile Image</title>
     </head>
     <body>
-        <h1><a href="../index.html">To index</a></h1>
+        <h1><a href="index.html">To index</a></h1>
         <img src="<?=$SOURCE?>" alt="이미지가 없다옹"/>
     </body>
 </html>
